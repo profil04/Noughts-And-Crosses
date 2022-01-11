be54c85b69c91004e1f00be420e15b45a72a5a79
profil04 <prorok.filip@hotmail.com>
2022-01-11 18:42:08 +0100

    added button state changer (x or o)

--- src/components/SmallField.js
+++ src/components/SmallField.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
 import '../App.css';
 
+let currentPlayer = 1;
+
 export default class SmallField extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            player: ""
+        }
+    }
+
+    handleClick = (player) => {
+        if (player == 1){
+            this.setState({
+                player: "x"
+            })
+        }else if (player == 2){
+            this.setState({
+                player: "o"
+            })        
+        }
+    }
+
+    changePlayer = (player) => {
+        if (player == 1){
+            return 2;
+        }else if (player == 2){
+            return 1;       
+        }
+    }
+
     render() {
         return (
             <div>
-                <button></button>
+                <button onClick={() => {
+                    this.handleClick(currentPlayer);
+                    currentPlayer = this.changePlayer(currentPlayer);
+                }}>{this.state.player}</button>
             </div>
         )
     }
