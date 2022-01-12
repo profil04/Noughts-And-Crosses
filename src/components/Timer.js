cea77386e0a7305d087ce0507532514c470526c3
KAWUH <93121540+KAWUH@users.noreply.github.com>
2022-01-12 20:36:15 +0100

    Added Timer functionality
    - added functionality to Timer.js
    - styled Timer in App.css file

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -7,15 +7,76 @@ export default class Timer extends Component {
         super(props)
     
         this.state = {
-             seconds: 0
+             active: 0,
+             seconds: 0,
+             minutes: 0,
+             hours: 0
         }
     }
     
+    startGameHandler = () => {
+        this.setState({
+            active: 1
+        },
+        () => 
+        setInterval(() => {
+            if (this.state.seconds === 59) {
+                this.setState({
+                    minutes: this.state.minutes + 1,
+                    seconds: -1
+                })
+            }
+
+            if (this.state.minutes === 59) {
+                this.setState({
+                    hours: this.state.hours + 1,
+                    minutes: 0
+                })
+            }
+
+            this.setState({seconds: this.state.seconds + 1})
+        }, 1000)
+        )
+    }
+
 
     render() {
-        return (
+
+        let hr, min, sec
+
+        if (this.state.seconds < 10) {
+            sec = '0' + this.state.seconds
+        }
+        else {
+            sec = this.state.seconds
+        }
+
+        if (this.state.minutes < 10) {
+            min = '0' + this.state.minutes
+        }
+        else {
+            min = this.state.minutes
+        }
+
+        if (this.state.hours < 10) {
+            hr = '0' + this.state.hours
+        }
+        else {
+            hr = this.state.hours
+        }
+
+
+        return this.state.active ? (
             <div className='timer'>
-                <button>PLAY</button>
+                <div className='timeCounter'>
+                    <div className='timerPart'>{hr + ' : '}</div>
+                    <div className='timerPart'>{min + ' : '}</div>
+                    <div className='timerPart'>{sec}</div>
+                </div>
+            </div>  
+        ) : (
+            <div className='timer'>
+                <button onClick={this.startGameHandler} className='gameStarter'>PLAY</button>
             </div>
         )
     }
